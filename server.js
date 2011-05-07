e97eb8f95e58e0e421276bf05425683f0228e36d
Kevan Davis <kdavis@gilt.com>
2011-05-07 09:25:33 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('GitSquare is Coming Soon!\n');
+}).listen(8124, "127.0.0.1");
+
+console.log('Server running at http://127.0.0.1:8124/');
