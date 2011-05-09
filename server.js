36f7c9aaf6321f196362da48545291535b7db725
Kevan Davis <kdavis@gilt.com>
2011-05-09 15:01:45 -0700

    well... duostack doesn't like webworkers... so...

--- server.js
+++ server.js
@@ -2,20 +2,9 @@ var
   http = require("http"),
   _ = require("underscore"),
   express = require("express"),
+  processor = require("./lib/processor"),
 
-  app = express.createServer(),
-  worker_thread = new (require("webworker")).Worker(process.cwd() + "/worker_thread.js");
-
-worker_thread.onmessage = function (msg) {
-  console.log(msg);
-  if ("log" === msg.type) {
-    console.log.apply(null, msg.args);
-  }
-};
-
-process.on("exit", function () {
-  worker_thread.terminate();
-});
+  app = express.createServer();
 
 app.use(express.bodyParser());
 
@@ -29,15 +18,9 @@ app.get("/", function(req, res) {
 app.all("/github_receive", function (req, res) {
   res.send(200);
 
-  // Worker thread does the hard work of storing in the db, calculating mayors and badges.
-  // I can't find a way to send a message back to the committer via github's post-receive
-  //   so there's no good reason to do this synchronously.
-  worker_thread.postMessage({
-    type: "payload",
-    payload: JSON.parse(req.param("payload"))
-  });
+  processor.processPayload(JSON.parse(req.param("payload")));
 });
 
 app.listen(9876);
 
-console.log("Web Thread started");
+console.log("Gitsquare started");
